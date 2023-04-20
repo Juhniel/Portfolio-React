e88c335d104e172178c0df76fb9dd8d24478bfc4
Juhniel <juhn.sweden@gmail.com>
2023-04-20 23:05:59 +0200

    added components and social icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Projects from "./components/Projects"
 import ProjectData from "../src/data/ProjectData"
 import SocialMedia from "./components/SocialMedia"
+import Footer from "./components/Footer"
 
 export default function App() {
   const projectArray = Object.values(ProjectData);
@@ -14,6 +15,7 @@ export default function App() {
         {projectArray.map(project => <Projects key={project.id} name={project.name} img={project.img} 
         description={project.description}/>)}
         <SocialMedia/>
+        <Footer/>
       </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import SocialMedia from "./SocialMedia"
+
+export default function Footer() {
+    return(
+        <footer>
+            <p>Email: contact@juhnkim.com</p>
+            <SocialMedia/>
+        </footer>
+    )
+}
