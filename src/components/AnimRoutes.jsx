2abc955f0f0a99d0d9dc0485c6f586cd946e146d
Juhniel <juhn.sweden@gmail.com>
2023-05-03 01:22:23 +0200

    transitions about

--- src/components/AnimRoutes.jsx
+++ src/components/AnimRoutes.jsx
@@ -1,22 +1,24 @@
-import Home from "../pages/Home"
-import About from "../pages/About"
-import Portfolio from "../pages/Portfolio"
-import Contact from "../pages/Contact"
+import React from 'react';
+// import pages
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Portfolio from '../pages/Portfolio';
+import Contact from '../pages/Contact';
 
-import { Routes, Route, useLocation} from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
+
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 export default function AnimRoutes() {
-    const location = useLocation();
-
-    return (
-        <AnimatePresence initial={true} mode="wait">
-            <Routes key={location.pathname} location={location}>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/portfolio" element={<Portfolio />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </AnimatePresence>
-    )
+  const location = useLocation();
+  return (
+    <AnimatePresence initial={true} mode="wait">
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,35 +3,51 @@ import React from "react";
 import WomanImg from "../img/about/woman.png";
 
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion"
+import { transition1 } from "../transitions";
 
 export default function About() {
   return (
-    <section className="section">
-      <div className="container mx-auto h-full relative">
+    <motion.section
+      initial={{ opacity: 0, y: '100%' }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: '100%' }}
+      transition={transition1}
+      className='section'
+    >
+      <div
+        className='container mx-auto h-full relative'
+      >
         {/* text & img wrapper */}
-        <div className="flex flex-col lg:flex lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
-          {/* img */}
-          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
-            <img src={WomanImg} alt="" />
+        <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+          {/* image */}
+          <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+            <img src={WomanImg} alt='' />
           </div>
           {/* text */}
-          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
-            <h1 className="h1">About me</h1>
-            <p className="mb-12 max-w-sm">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-              <b>Ducimus
-              libero facilis</b> assumenda dolore saepe labore qui recusandae, quis
-              debitis rerum! Veritatis et eaque quam, laboriosam ipsam ex
-              aperiam facere tenetur.
+          <motion.div
+            initial={{ opacity: 0, y: '-80%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-80%' }}
+            transition={transition1}
+            className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'
+          >
+            <h1 className='h1'>About me</h1>
+            <p className='mb-12 max-w-sm'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              <b> Exercitationem, veritatis.</b> Veritatis illum aut,
+              reprehenderit sed dolorem dolore.
               <br />
               <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Recusandae, iure! Ullam, dolore? Eligendi, quam mollitia.
             </p>
-
-            <Link to={"/portfolio"} className="btn">View my work</Link>
-          </div>
+            <Link to={'/portfolio'} className='btn'>
+              View my work
+            </Link>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
-}
+};
