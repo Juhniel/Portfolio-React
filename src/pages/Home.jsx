b7a84600ac22fdbe5ac326fa9cfc33d657ff5560
Juhniel <juhn.sweden@gmail.com>
2023-05-03 00:35:26 +0200

    transitions home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,53 @@
-import React from "react"
+import React from "react";
 
-import WomanImg from "../img/home/woman.png"
+import WomanImg from "../img/home/woman.png";
 
-import  {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import {motion} from "framer-motion"
-import {} from "../transitions"
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 export default function Home() {
-    return(
-        <>
-            <section className="section bg-blue-200">
-                <div className="container mx-auto h-full relative">
-                    {/* text & img wrapper */}
-                    <div className="flex flex-col justify-center">
-                        {/* text */}
-                        <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
-                            <h1 className="h1">
-                                photographer <br/> & film maker 
-                            </h1>
-                            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
-                                Los Angeles, USA
-                            </p>
-                            <Link to={"/contact"} className="btn mb-[30px]">
-                                hire me
-                            </Link>
-                        </div>
-                        <div className="flex justify-end max-h-96 lg:max-h-max">
-                            <div className="relative lg:-right-40 overflow-hidden">
-                                <img src={WomanImg} alt="" />
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ transition1 }}
+      className="section bg-blue-200"
+    >
+      <div className="container mx-auto h-full relative">
+        {/* text & img wrapper */}
+        <div className="flex flex-col justify-center">
+          {/* text */}
+          <motion.div 
+            initial={{ opacity: 0, y: '-50%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-50% '}}
+            transition={{ transition1 }}
+            className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+            <h1 className="h1">
+              photographer <br /> & film maker
+            </h1>
+            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
+              Los Angeles, USA
+            </p>
+            <Link to={"/contact"} className="btn mb-[30px]">
+              hire me
+            </Link>
+          </motion.div>
+          <div className="flex justify-end max-h-96 lg:max-h-max">
+            <motion.div 
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+                transition={{ transition1 }}
+                className="relative lg:-right-40 overflow-hidden">
+              <img src={WomanImg} alt="" />
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 }
