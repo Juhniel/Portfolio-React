d614962650d491551292c45425a36be0f1ba90a0
Juhniel <juhn.sweden@gmail.com>
2023-05-03 01:29:32 +0200

    animate portfolio contact

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,46 +6,79 @@ import Image3 from "../img/portfolio/3.png";
 import Image4 from "../img/portfolio/4.png";
 
 import { Link } from "react-router-dom";
+
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
+
 export default function Portfolio() {
   return (
-    <section className="section">
-      <div className="container mx-auto h-full relative">
-        <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8">
+    <motion.section
+      initial={{ opacity: 0, y: '100%' }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: '100%' }}
+      transition={transition1}
+      className='section'
+    >
+      <div className='container mx-auto h-full relative'>
+        <div className='flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8'>
           {/* text */}
-          <div className="flex flex-col lg:items-start">
-            <h1 className="h1">Portfolio</h1>
-            <p className="mb-12 max-w-sm">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              <b>Ducimus libero facilis</b> assumenda dolore saepe labore qui
-              recusandae, quis debitis rerum! Veritatis et eaque quam,
-              laboriosam ipsam ex aperiam facere tenetur.
+          <motion.div
+            initial={{ opacity: 0, y: '-80%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-80%' }}
+            transition={transition1}
+            className='flex flex-col lg:items-start'
+          >
+            <h1 className='h1'>Portfolio</h1>
+            <p className='mb-12 max-w-sm'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              <b> Exercitationem, veritatis.</b> Veritatis illum aut,
+              reprehenderit sed dolorem dolore.
               <br />
               <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Blanditiis, quas.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Recusandae, iure! Ullam, dolore? Eligendi, quam mollitia.
             </p>
-            <Link to={"/contact"} className="btn mb-[30px] mx-auto lg:mx-0">
+            <Link to={'/contact'} className='btn mb-[30px] mx-auto lg:mx-0'>
               Hire me
             </Link>
-          </div>
+          </motion.div>
           {/* image grid */}
-          <div className="grid grid-cols-2 lg:gap-2">
+          <div
+            className='grid grid-cols-2 lg:gap-2'
+          >
             {/* image */}
-            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-                <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image1} alt="" />
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img
+                className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500'
+                src={Image1}
+                alt=''
+              />
             </div>
-            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-                <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image2} alt="" />
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img
+                className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500'
+                src={Image2}
+                alt=''
+              />
             </div>
-            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-                <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image3} alt="" />
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img
+                className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500'
+                src={Image3}
+                alt=''
+              />
             </div>
-            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-                <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image4} alt="" />
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img
+                className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500'
+                src={Image4}
+                alt=''
+              />
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
-}
+};
